6cbe0d2ab42e7ef2d02d3c150463845136d1f1a9
Jaffer1999 <148843932+JafferTan@users.noreply.github.com>
2024-11-27 02:06:24 +0800

    feature: 新增go-swagger框架和gin框架

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	docs "JafferSimpleText2SQL/docs"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"       // swagger embed files
+	"github.com/swaggo/gin-swagger" // gin-swagger middleware
+	"net/http"
+)
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "hello baojie my baby")
+}
+
+func main() {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", Helloworld)
+		}
+
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
+
+}
